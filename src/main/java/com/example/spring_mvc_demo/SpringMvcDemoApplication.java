8a5bec4d40e0e065017d90cc684fcaadb4680af8
Ibragim Kuliev <i.kuliev@tarent.de>
2019-02-21 16:16:39 +0100

    application skeleton from spring-initializr.io

---
+++ src/main/java/com/example/spring_mvc_demo/SpringMvcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcDemoApplication.class, args);
+    }
+
+}
